708aace15f8b0b285cc292f781181181dde1617f
Mahdi <mahdiiporheydarii@gmail.com>
2024-01-04 15:09:20 +0330

    Plotly dash app Added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import networkx as nx
+import dash
+from dash.dependencies import dcc, html
 import matplotlib.pyplot as plt
-from src.plot import plot_family_tree
+from src.plot import plot_family_tree, graph_to_plotly
 from src.Tree import family
 
 fam = family()
@@ -10,3 +12,14 @@ pos = plot_family_tree(fam.root, graph=graph)
 labels = {node: node.hash for node in graph.nodes()}
 nx.draw(graph, pos=pos, with_labels=True, labels=labels, node_size=1500, node_color="#33FFC1", font_size=10)
 plt.show()
+
+
+app = dash.Dash(__name__)
+
+
+app.layout = html.Div([
+    dcc.Graph(id='family-tree-graph', figure=graph_to_plotly(graph, pos)),
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
