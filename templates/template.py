9113d261526f129d3541570ee77fedc3b99074ef
mehrmof <mofidimehrnoosh1@gmail.com>
2024-01-24 12:32:07 +0330

    add icon git

--- templates/template.py
+++ templates/template.py
@@ -1,14 +1,20 @@
 from dash import html
 import dash_bootstrap_components as dbc
 
+github_link = html.A(
+    html.Img(src='https://icons.getbootstrap.com/assets/icons/github.svg', style={'height': '25px', 'width': '25px',}),
+    href='https://github.com/MahdiPorheydarii/Mysterious-Family',
+)
+
 navbar = dbc.Navbar(
     [
-        dbc.NavbarBrand("Mysterious Family",style={'margin-left' : '13px'}),
+        dbc.NavbarBrand("Mysterious Family", style={'margin-left': '13px'}),
         dbc.Nav(
             [
                 dbc.NavItem(dbc.NavLink("Home", href="/")),
                 dbc.NavItem(dbc.NavLink("Import Tree", href="/import_tree")),
                 dbc.NavItem(dbc.NavLink("Info", href="/info")),
+                dbc.NavItem(github_link),
             ],
             className="ml-auto",
             navbar=True,
